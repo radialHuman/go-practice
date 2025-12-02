2afca7eed6e240c110d43d0325e01212b13391ce
radialhuman <rd201431951@gmail.com>
2025-12-02 12:21:13 +0530

    1 : hello world, executable

--- practice/hello/main.go
+++ practice/hello/main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 // only one main function can exist
-func main(){
-	fmt.Println("Hello")
+func main() {
+	fmt.Println("hello")
 }
+
+// build executeable (run anywhere)
+// go build -o executeable_name main.go
+
+// run this fie (just run here)
+// go run main.go
