30f0d15fd452983ef06d7aebe6479d7a50900eb0
radialhuman <rd201431951@gmail.com>
2025-12-06 09:31:52 +0530

    restructuring folders

--- tutorial-webapp/1_basics/14_tests/main.go
+++ tutorial-webapp/1_basics/14_tests/main.go
@@ -18,7 +18,7 @@ func main() {
 func divide(x, y float32) (float32, error) {
 	var result float32
 	if y == 0 {
-		return result, errors.New("Cant divide by 0")
+		return result, errors.New("cant divide by 0") // error string shouold not have captial letetrs
 	}
 	result = x / y
 	return result, nil
